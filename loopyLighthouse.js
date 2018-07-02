a60ff0d50a39e1b2e25c0e8c980b5a1454d8926a
Delson Tan <delsonwtan@gmail.com>
2018-07-02 21:27:05 +0000

    Refactored loopyLighthouse.js

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,26 +1,15 @@
 function loopyLighthouse() {
-  var n = 100;
-  var d3 = 0;             // variable to check if n is divisible by 3
-  var d4 = 0;             // variable to check if n is divisible by 4
-  while (n <= 200) {
-    d3 = n % 3;
-    d4 = n % 4;
-    if (d3 == 0) {                      // case: n is divisible by 3
-      if (d4 == 0) {                    // case: n is divisible by 3 and 4
-        console.log("LoopyLighthouse");
-      }
-      else
-        console.log("Loopy");           // case: n is divisible by 3 but not 4
+
+  for (var n = 100; n <= 200; n++) {
+    var result = "";
+    if (n % 3 === 0) {
+      result += "Loopy";
     }
-    else if (d4 == 0) {                 // case: n is divisible by 4 but not 3
-      console.log("Lighthouse");
+    if (n % 4 === 0) {
+      result += "Lighthouse";
     }
-    else {
-      console.log(n);                   // case: n is not divisible by 3 or 4
-    }
-    n++;
+    console.log(result === "" ? n : result);
   }
-  return;
 }
 
 loopyLighthouse();
